37749f2e75b77fa473ce19add73168a5ade3bbdd
yoonisali <yoonismahamoudali@gmail.com>
2022-10-17 14:50:34 -0700

    add code to javascript
    co-authored-by: David Gamble <daviddgamble@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
 
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-
-window.alert(add(number1, number2));
+let addResult = ('The addition of your numbers equals ' + add(number1, number2) + '.');
+let subtractResult = ('The subtraction of your numbers equals ' + subtract(number1, number2) + '.');
+let multiplyResult = ('The multiplication of your numbers equals ' + multiply(number1, number2) + '.');
+let divideResult = ('The division of your numbers equals ' + divide(number1, number2) + '.');
+window.alert(addResult + subtractResult + multiplyResult + divideResult);
